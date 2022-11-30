1f1e92fe399e5bafd52ec25ac0e986feaa61257e
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-11-30 09:34:47 -0300

    day-1

---
+++ day_1.py
@@ -0,0 +1,35 @@
+# núemros variam entre inteiros e floats
+
+# int
+a = 5
+b = 10
+c = -15
+
+# floats
+d = 3.1
+e = 4.0
+f = -5.1234
+
+# print function
+print("babadi")
+
+# operadores aritméticos
+a + b
+c - d
+d / a
+f * e
+
+# exercício
+idade = 19
+print(idade)
+meses = 12 * 27
+dias = 365 * 27
+semanas = 52 * 27
+print(f"27 anos têm {meses} meses, {dias} dias, {semanas} semanas")
+
+
+def area_circulo(pi, raio):
+    return pi * raio**2
+
+
+print(area_circulo(3.14, 5))
