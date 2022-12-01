bfb179a4865788f043e37237480fddb7e19ab6ab
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-12-01 10:19:58 -0300

    day-3 project

--- earnings_calculator/project.py
+++ earnings_calculator/project.py
@@ -1,3 +1,5 @@
+# day-3 project
+
 employee_name = input("Type the employee name: ").title().strip()
 hourly_wage = int(input("type the hourly wage: "))
 hours_worked = int(input("type the hours worked: "))
