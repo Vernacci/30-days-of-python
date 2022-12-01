3bec9b5fc3c7fe572399b60a7b6b9600e9898a3f
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-12-01 10:16:53 -0300

    earnings project

---
+++ earnings_calculator/project.py
@@ -0,0 +1,5 @@
+employee_name = input("Type the employee name: ").title().strip()
+hourly_wage = int(input("type the hourly wage: "))
+hours_worked = int(input("type the hours worked: "))
+
+print(f"Employee {employee_name}, has earned ${hourly_wage * hours_worked} this week!")
