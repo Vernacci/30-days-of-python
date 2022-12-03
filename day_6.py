81f3d3a712de7ae7b4ca26f5292cf97632d79054
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-12-03 13:27:10 -0300

    added exercise

--- day_6.py
+++ day_6.py
@@ -12,3 +12,16 @@ for movie in movies:
         break
 
 print(range(3, 10))
+print("-----------------------------------")
+# exercícios
+
+employees = [
+    ("Rolf Smith", 35, 8.75),
+    ("Anne Pun", 30, 12.50),
+    ("Charlie Lee", 50, 15.50),
+    ("Bob Smith", 20, 7.00),
+]
+
+for employee in employees:
+    total_pay = employee[1] * employee[2]
+    print(f"employee: {employee[0]} is due to be paid {total_pay}")
