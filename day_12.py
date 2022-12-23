a9c1fb5b1f84fd96384be4b370a28da10e922da5
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-12-23 09:22:34 -0300

    day-12

---
+++ day_12.py
@@ -0,0 +1,15 @@
+# functions
+def get_even_numbers(amount: int):
+    for number in range(1, amount + 1):
+        print(number * 2)
+
+
+get_even_numbers(30)
+
+
+def x_print(output, amount):
+    for _ in range(amount + 1):
+        print(output)
+
+
+x_print('Fortnaite ou la babadi???', 12)
