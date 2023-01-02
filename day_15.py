15b5ba43da9ce733fbe6bb92ddeafe50893234bd
Victor Vernasqui <victorvernasqui12@gmail.com>
2023-01-02 10:07:51 -0300

    Update day_15.py

--- day_15.py
+++ day_15.py
@@ -3,3 +3,20 @@ names = ['frezin', 'vito', 'emanuweb', 'yanal', 'mirongas']
 
 names = [name.title() for name in names]
 print(names)
+
+
+# exercicios
+numbers = [1, 2, 3, 4, 5]
+squares = [number ** 2 for number in numbers]
+
+print(squares)
+
+movie = {
+    "title": "thor: ragnarok",
+    "director": "taika waititi",
+    "producer": "kevin feige",
+    "production_company": "marvel studios"
+}
+
+updated_movie = {key: value.title() for key, value in movie.items()}
+print(updated_movie)
