c4e7c008a7827dca18e89345511cef15f409d61e
Victor Vernasqui <victorvernasqui12@gmail.com>
2023-01-24 09:19:30 -0300

    Update day_20.py

--- day_20.py
+++ day_20.py
@@ -21,10 +21,10 @@ total = map(add, odds, evens)
 print(*total, sep=', ')
 
 
-names = ['ellie', 'joel', 'tess', 'tommy']
+# names = ['ellie', 'joel', 'tess', 'tommy']
 
-title_names = map(methodcaller("title"), names)
-print(*title_names)
+# title_names = map(methodcaller("title"), names)
+# print(*title_names)
 
 
 numbers = [1, 56, 3, 5, 24, 19, 88, 37]
@@ -37,3 +37,22 @@ def is_even(number):
 
 numbers = [1, 56, 3, 5, 24, 19, 88, 37]
 even_numbers = filter(is_even, numbers)
+
+print('---------------------------------')
+# exercicios
+
+humpty_dumpty = [
+    "  Humpty Dumpty sat on a wall,  ",
+    "Humpty Dumpty had a great fall;     ",
+    "  All the king's horses and all the king's men ",
+    "    Couldn't put Humpty together again."
+]
+
+mapped_text = map(methodcaller("strip"), humpty_dumpty)
+print(*mapped_text, sep='\n')
+
+
+names = ("bob", "Christopher", "Rachel", "MICHAEL", "jessika", "francine")
+
+filtered_names = [name for name in names if len(name) < 8]
+print(filtered_names)
